e8da1d397a75b2c3eccce1955f7f90a6fe96e8b3
Chantha makara <chantha.makara@institute.pse.ngo>
2026-01-30 13:06:45 +0700

    add icon on metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,24 @@
+// src/app/layout.tsx
+
 import "./globals.css";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import AuthProvider from "@/components/AuthProvider"; //
+import AuthProvider from "@/components/AuthProvider";
 
-// Initialize the font
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "700"],
 });
 
 export const metadata: Metadata = {
-  title: "Cambodian Craft Beer Crown 2025",
-  description: "A description of my awesome Next.js app.",
+  title: "Cambodian Craft Beer Crown 2026",
+  description:
+    "Help us crown the best brew at the Great Cambodian Craft Beer Festival!",
+  icons: {
+    icon: "/images/BeerFestival.png", // This points to your logo in the images folder
+  },
 };
 
 export default function RootLayout({
@@ -26,7 +31,6 @@ export default function RootLayout({
       <body
         className={`${poppins.className} flex flex-col min-h-screen bg-amber-50 text-gray-900`}
       >
-        {/* Wrapping inside AuthProvider allows all components to use session data */}
         <AuthProvider>
           <Header />
           <main className="flex-1 container mx-auto p-4">{children}</main>
